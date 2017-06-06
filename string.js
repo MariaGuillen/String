f5bc21c7b21a3d82824f377482f7dab9e622806f
Maria G <mdlaguillen@cosola.org>
2017-06-06 10:00:46 -0400

    html, css y js
    Subiendo archivos de html, css y js

---
+++ string.js
@@ -0,0 +1,14 @@
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+if(hourNow>18)
+	{greeting = "It's Sarah's dinner";}
+	else if(hourNow>12)
+		{greeting = "It's Sarah's lunch!";}
+	else if(hourNow>0)
+		{greeting = "It's Sarah's breakfast!";}
+	else
+		{greeting = "It's Sarah's snak!";}
+
+document.write('<h2>'+greeting+'</h2>');
